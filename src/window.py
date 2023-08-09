20b09d2363e381fcf9cfb64b6cf43add84975824
DaemonWhite <matheo.daemon@gmail.com>
2023-08-09 16:56:27 +0200

    sous modules dependances

--- src/window.py
+++ src/window.py
@@ -2,6 +2,8 @@
 from gi.repository import Adw
 from gi.repository import Gtk
 
+from LibPictureSorter import Picture_sorter
+
 @Gtk.Template(resource_path='/fr/daemonwhite/sortpictureresolve/window.ui')
 class SortpictureresolveGuiWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'SortpictureresolveGuiWindow'
