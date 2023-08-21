98fd8d5ca6d8ee742f5af01f8950541397b4dd82
DaemonWhite <matheo.daemon@gmail.com>
2023-08-21 16:37:23 +0200

    add : controller rectangle

--- src/drawratiocontroller/__init__.py
+++ src/drawratiocontroller/__init__.py
@@ -5,7 +5,7 @@ from .rectangle_draw import RectangleDraw
 
 #TODO Ajouter la gestion des coulleur
 
-#TODO Ajouter le depalcement des rectangles
+#TODO Ajouter la non revesirité des rectangles
 
 class DrawRatioController(Gtk.DrawingArea):
 
@@ -14,14 +14,15 @@ class DrawRatioController(Gtk.DrawingArea):
             vexpand=True,
             hexpand=True
         )
+        self.__rectangle_selected = -1
+        self.__is_drag = False
         self.__grab_cursor = Gdk.Cursor.new_from_name("grab", None)
         self.__default_cursor = Gdk.Cursor.new_from_name("default", None)
 
         drag = Gtk.GestureDrag.new()
         drag.connect("drag_begin", self.drag_begin)
         drag.connect("drag_update", self.drag_update)
-        # self.connect("motion", self.mouse_update)
-        #drag.connect("update", self.mouse_update)
+        drag.connect("drag_end", self.drag_end)
 
         mouse = Gtk.EventControllerMotion.new()
         mouse.connect("motion", self.mouse_update)
@@ -31,7 +32,7 @@ class DrawRatioController(Gtk.DrawingArea):
         self.__line = ""
         self.__rectangle_list = list()
         self.add_rectangle(3,3, 53,53)
-        self.add_rectangle(60,60, 150,150)
+        self.add_rectangle(80,80, 150,150)
 
 
     def add_rectangle(
@@ -51,28 +52,42 @@ class DrawRatioController(Gtk.DrawingArea):
                 radius
             )
         )
+        RectangleDraw.count_rectangle += 1
 
     def mouse_update(self, motion,  x, y):
         for rc in self.__rectangle_list:
             if rc.cursor_left_is_select(x, y):
+                self.__is_drag = True
                 self.set_cursor(self.__grab_cursor)
+                self.__rectangle_selected = rc.get_id()
             elif rc.cursor_rigth_is_select(x, y):
+                self.__is_drag = True
                 self.set_cursor(self.__grab_cursor)
-            else:
+                self.__rectangle_selected = rc.get_id() + 0.5
+            elif not self.__is_drag:
                 self.set_cursor(self.__default_cursor)
+                self.__rectangle_selected = -1
 
-
+    def drag_end(self, gesture, x, y):
+        selected = int(self.__rectangle_selected)
+        self.__rectangle_list[selected].apply_offset()
+        self.__is_drag = False
+        self.__rectangle_selected = -1
 
     def drag_update(self, gesture, x, y):
-        print(x, y)
+        selected = int(self.__rectangle_selected)
+        if self.__is_drag:
+            if self.__rectangle_selected == (selected + 0.5):
+                self.__rectangle_list[selected].set_rigth_offset(x, y)
+            else:
+                self.__rectangle_list[selected].set_left_offset(x, y)
+
         self.queue_draw()
 
     def drag_begin(self, gesture, x, y):
-        print(x,y)
         self.queue_draw()
 
     def circle(self, c, x, y, r):
-        print(x,y)
         c.arc(x, y, r, 0, 2*math.pi)
         c.set_line_width(3)
         c.set_source_rgb(0,150,0)
@@ -97,5 +112,5 @@ class DrawRatioController(Gtk.DrawingArea):
             )
             c.stroke()
             self.circle(c, left_X, left_Y, radius)
-            self.circle(c, left_X+rigth_X, left_X+rigth_X, radius)
+            self.circle(c, left_X+rigth_X, left_Y+rigth_Y, radius)
             
--- src/drawratiocontroller/rectangle_draw.py
+++ src/drawratiocontroller/rectangle_draw.py
@@ -1,7 +1,8 @@
 
 class RectangleDraw(object):
     radius = float(0)
-    count_rectagle = 0
+    count_rectangle = 0
+    DEFAULT_OFSSET = 0
     def __init__(self,
             x_left,
             y_left,
@@ -9,13 +10,15 @@ class RectangleDraw(object):
             y_rigth,
             radius
         ):
+        self.__rectangle_id = self.count_rectangle
         self.radius = radius
-        self.count_rectagle += 1
 
         self.__x_left = x_left
         self.__y_left = y_left
         self.__x_rigth = x_rigth
         self.__y_rigth = y_rigth
+
+        self.reset_offset()
 
         self.__cursor_left = [
             self.__x_left - self.radius,
@@ -48,6 +51,9 @@ class RectangleDraw(object):
     def cursor_rigth_is_select(self, x, y):
         return self.cursor_select(self.__cursor_rigth, x, y)
 
+    def get_id(self):
+        return self.__rectangle_id
+
     def get_cursot_left(self):
         return self.__cursor_left.copy()
 
@@ -61,13 +67,53 @@ class RectangleDraw(object):
         return self.radius
 
     def get_x_left(self):
-        return self.__x_left
+        return self.__x_left + self.__x_left_offset
 
     def get_y_left(self):
-        return self.__y_left
+        return self.__y_left + self.__y_left_offset
 
     def get_x_rigth(self):
-        return self.__x_rigth
+        return self.__x_rigth + self.__x_rigth_offset
 
     def get_y_rigth(self):
-        return self.__y_rigth
+        return self.__y_rigth + self.__y_rigth_offset
+
+    def set_left_offset(self, x, y):
+        self.__x_left_offset = x
+        self.__y_left_offset = y
+        self.__x_rigth_offset = -x
+        self.__y_rigth_offset = -y
+
+    def set_rigth_offset(self, x, y):
+        self.__x_rigth_offset = x
+        self.__y_rigth_offset = y
+
+    def apply_offset(self):
+        self.__x_left += self.__x_left_offset
+        self.__y_left += self.__y_left_offset
+        self.__x_rigth += self.__x_rigth_offset
+        self.__y_rigth += self.__y_rigth_offset
+        self.define_drag_cursor()
+        self.reset_offset()
+
+    def reset_offset(self):
+        self.__x_left_offset = self.DEFAULT_OFSSET
+        self.__y_left_offset = self.DEFAULT_OFSSET
+        self.__x_rigth_offset = self.DEFAULT_OFSSET
+        self.__y_rigth_offset = self.DEFAULT_OFSSET
+
+    def define_drag_cursor(self):
+        self.__cursor_left = [
+            self.__x_left - self.radius,
+            self.__x_left + self.radius,
+            self.__y_left - self.radius,
+            self.__y_left + self.radius,
+        ]
+
+        self.__cursor_rigth = [
+            self.__x_left + self.__x_rigth - self.radius,
+            self.__x_left + self.__x_rigth + self.radius,
+            self.__y_left + self.__y_rigth - self.radius,
+            self.__x_left + self.__y_rigth + self.radius,
+        ]
+        
